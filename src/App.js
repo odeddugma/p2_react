63e0e1a35d6c6435b88790dfcd65166484ec9d3e
Oded Dugma <odeddugma@gmail.com>
2023-04-05 16:56:09 +0300

    get details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const usersURL = "https://jsonplaceholder.typicode.com/users";
+const todosURL = "https://jsonplaceholder.typicode.com/todos";
+const postsURL = "https://jsonplaceholder.typicode.com/posts";
 
 function App() {
-	return <div className="App"></div>;
+	const [users, setUsers] = useState([]);
+	const [todos, setTodos] = useState([]);
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		console.log("useEffect");
+		const getUsers = async () => {
+			const response = await axios.get(usersURL);
+			setUsers(response.data);
+		};
+		const getTodos = async () => {
+			const response = await axios.get(todosURL);
+			setTodos(response.data);
+		};
+		const getPosts = async () => {
+			const response = await axios.get(postsURL);
+			setPosts(response.data);
+		};
+		getUsers();
+		getTodos();
+		getPosts();
+	}, []);
+
+	console.log("Render");
+	return (
+		<div className="App">
+			<div>main</div>
+			<div>side</div>
+		</div>
+	);
 }
 
 export default App;
