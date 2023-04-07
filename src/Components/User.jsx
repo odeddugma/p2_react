0a8050fd517577733402fe0bf65f27142957bc45
Oded Dugma <odeddugma@gmail.com>
2023-04-07 15:23:20 +0300

    show other data

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const User = ({ user, callback }) => {
 	const [data, setData] = useState(user);
+	const [showOtherData, setShowOtherData] = useState(false);
 
 	//TODO: Remove after testing
 	if (user.id === 2) {
@@ -13,8 +14,6 @@ const User = ({ user, callback }) => {
 	);
 
 	const color = haveUncompletedTask ? "red" : "green";
-
-	//console.log(callback);
 
 	const updateteUser = () => {
 		callback.updateUser(data);
@@ -48,10 +47,31 @@ const User = ({ user, callback }) => {
 					onInput={(e) => setData({ ...user, email: e.target.value })}
 				/>
 			</p>
+
 			<div>
+				<button
+					onMouseOver={() => setShowOtherData(true)}
+					onClick={() => setShowOtherData(false)}
+				>
+					Otehr Data
+				</button>
 				<button onClick={updateteUser}>Update</button>
 				<button onClick={() => deleteUser(user.id)}>Delete</button>
 			</div>
+
+			{showOtherData && (
+				<div className="other-data-container">
+					<p>
+						Street: <input type="text" placeholder={user.address.street} />
+					</p>
+					<p>
+						City: <input type="text" placeholder={user.address.city} />
+					</p>
+					<p>
+						Zip Code: <input type="text" placeholder={user.address.zipcode} />
+					</p>
+				</div>
+			)}
 		</article>
 	);
 };
