3052f7729857546a0cd87fccbe96e8a1f153aad3
Oded Dugma <odeddugma@gmail.com>
2023-04-07 12:06:10 +0300

    fetch data

--- src/App.js
+++ src/App.js
@@ -3,34 +3,40 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const usersURL = "https://jsonplaceholder.typicode.com/users";
-const todosURL = "https://jsonplaceholder.typicode.com/todos";
+const todosURL = "https://jsonplaceholder.typicode.com/todos/";
 const postsURL = "https://jsonplaceholder.typicode.com/posts";
 
 function App() {
 	const [users, setUsers] = useState([]);
-	const [todos, setTodos] = useState([]);
-	const [posts, setPosts] = useState([]);
 
 	useEffect(() => {
 		console.log("useEffect");
-		const getUsers = async () => {
-			const response = await axios.get(usersURL);
-			setUsers(response.data);
+
+		const getUsersAndData = async () => {
+			const users = (await axios.get(usersURL)).data;
+			const todos = (await axios.get(todosURL)).data;
+			const posts = (await axios.get(postsURL)).data;
+
+			for (const user of users) {
+				user.todos = [];
+				for (const todo of todos) {
+					if (todo.userId === user.id) {
+						user.todos.push(todo);
+					}
+				}
+				user.posts = [];
+				for (const post of posts) {
+					if (post.userId === user.id) {
+						user.posts.push(post);
+					}
+				}
+			}
+			setUsers(users);
 		};
-		const getTodos = async () => {
-			const response = await axios.get(todosURL);
-			setTodos(response.data);
-		};
-		const getPosts = async () => {
-			const response = await axios.get(postsURL);
-			setPosts(response.data);
-		};
-		getUsers();
-		getTodos();
-		getPosts();
+
+		getUsersAndData();
 	}, []);
 
-	console.log("Render");
 	return (
 		<div className="App">
 			<div>main</div>
